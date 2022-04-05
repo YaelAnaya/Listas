55c2d78a9181b0ccb775d3e4c4915a7ad2e4e817
yaela <yael.anaya@uabc.edu.mx>
2022-04-05 08:04:09 -0700

    all methods finished

--- src/main/java/TesterListaCircular.java
+++ src/main/java/TesterListaCircular.java
@@ -1,13 +1,42 @@
 public class TesterListaCircular {
     public static void main(String[] args) {
-        var listaCircular = new CircularSinglyLinkedList<Integer>();
-        listaCircular.addFirst(2);
-        listaCircular.addLast(3);
-        listaCircular.addFirst(7);
-        //listaCircular.removeFirst();
+        var circularList = new CircularSinglyLinkedList<Integer>();
+        circularList.addFirst(6);
+        circularList.addLast(50);
+        circularList.add(2, 275);
+        circularList.add(1, 76);
+        circularList.addLast(8);
 
-        System.out.println(listaCircular.showList());
-        listaCircular.sort();
-        System.out.println(listaCircular.showListRecursive());
+        System.out.println(circularList.showListRecursive() +
+                "\n- Número de elementos: " + circularList.getSize() + "\n");
+
+        System.out.println("Buscar el index del elemento con valor 7: " + circularList.search(7) + "\n");
+        System.out.println("Buscar el index del elemento con valor 6: " + circularList.search(6) + "\n");
+
+        System.out.println("Remove = " +circularList.removeAt(2) + "\n");
+        System.out.println(circularList.showListRecursive() +
+                "\n- Número de elementos: " + circularList.getSize() + "\n");
+
+        circularList.sort();
+        System.out.println("Sorted: " + circularList.showListRecursive() +
+                "\n- Número de elementos: " + circularList.getSize() + "\n");
+
+        System.out.println("Remove First = " +circularList.removeFirst() + "\n");
+        System.out.println("Remove Last = " +circularList.removeLast() + "\n");
+
+        System.out.println(circularList.showListRecursive() +
+                "\n- Número de elementos: " + circularList.getSize() + "\n");
+
+        System.out.println("Remove = " +circularList.remove(2) + "\n");
+
+        circularList.addLast(3);
+        System.out.println(circularList.showListRecursive() +
+                "\n- Número de elementos: " + circularList.getSize() + "\n");
+
+        System.out.println("Remove = " +circularList.remove(8) + "\n");
+
+        System.out.println(circularList.showListRecursive() +
+                "\n- Número de elementos: " + circularList.getSize() + "\n");
+
     }
 }
--- src/main/java/TesterListaDoble.java
+++ src/main/java/TesterListaDoble.java
@@ -1,16 +1,42 @@
 public class TesterListaDoble {
     public static void main(String[] args) {
-        var listaDoble = new DoublyLinkedList<Integer>();
+        var doublyList = new DoublyLinkedList<Integer>();
+        doublyList.addFirst(1);
+        doublyList.addLast(30);
+        doublyList.add(2, 25);
+        doublyList.add(3, 7);
+        doublyList.addLast(8);
 
-        listaDoble.addFirst(2);
-        listaDoble.addLast(1);
-        listaDoble.addLast(3);
-        listaDoble.addFirst(0);
+        System.out.println(doublyList.showListRecursive() +
+                "\n- Número de elementos: " + doublyList.getSize() + "\n");
 
-        System.out.println(listaDoble.showList());
-        listaDoble.sort();
+        System.out.println("Buscar el index del elemento con valor 7: " + doublyList.search(7) + "\n");
+        System.out.println("Buscar el index del elemento con valor 6: " + doublyList.search(6) + "\n");
 
-        System.out.println(listaDoble.showListRecursive());
+        System.out.println("Remove = " +doublyList.removeAt(2) + "\n");
+        System.out.println(doublyList.showListRecursive() +
+                "\n- Número de elementos: " + doublyList.getSize() + "\n");
+
+        doublyList.sort();
+        System.out.println("Sorted: " + doublyList.showListRecursive() +
+                "\n- Número de elementos: " + doublyList.getSize() + "\n");
+
+        System.out.println("Remove First = " +doublyList.removeFirst() + "\n");
+        System.out.println("Remove Last = " +doublyList.removeLast() + "\n");
+
+        System.out.println(doublyList.showListRecursive() +
+                "\n- Número de elementos: " + doublyList.getSize() + "\n");
+
+        System.out.println("Remove = " +doublyList.remove(2) + "\n");
+
+        doublyList.addLast(3);
+        System.out.println(doublyList.showListRecursive() +
+                "\n- Número de elementos: " + doublyList.getSize() + "\n");
+
+        System.out.println("Remove = " +doublyList.remove(8) + "\n");
+
+        System.out.println(doublyList.showListRecursive() +
+                "\n- Número de elementos: " + doublyList.getSize() + "\n");
 
     }
 }
